ffb7a7bf5eecba397a331aefa5c5b863e887e2d5
Manten Devillé <manten.d@hotmail.com>
2020-11-18 11:34:49 +0100

    JS: Add if/else to compare random & input number

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -8,9 +8,16 @@
         //met functie
         let result = secretNumber(1,22);
         //zonder functie
-        let resultwithoutfunct = Math.floor(Math.random() * 21 + 1);
+        //let resultwithoutfunct = Math.floor(Math.random() * 21 + 1);
+        console.log(result);
 
+        if (theNumber == result) {
+            alert("Awesome! Your number " + theNumber + " was correct. You can be named many things, hungry not being one of them.")
+        } else {
+            alert("Bummer... You guessed " + theNumber + " and the secret number was " + result + ".")
+        }
     });
+
     function secretNumber(min, max) {
         return Math.floor(Math.random() * (max - min) + min);  
       }
