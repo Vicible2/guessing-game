06f6e555c4959c57511e33d12eb417f8da65733e
Manten Devillé <manten.d@hotmail.com>
2020-11-18 11:25:58 +0100

    JS: add function to generate random between 1-22

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -5,8 +5,15 @@
 
         let theNumber = document.getElementById("theNumber").value
         console.log(theNumber);
-    });
+        //met functie
+        let result = secretNumber(1,22);
+        //zonder functie
+        let resultwithoutfunct = Math.floor(Math.random() * 21 + 1);
 
+    });
+    function secretNumber(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);  
+      }
 
 
 
