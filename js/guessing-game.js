1b93866c2af8e3d8a9088251cb2478a8f49fc642
Manten Devillé <manten.d@hotmail.com>
2020-11-18 10:58:47 +0100

    JS: add stop to refresh for submit button

---
+++ js/guessing-game.js
@@ -0,0 +1,13 @@
+(function () {
+
+    document.getElementById("run").addEventListener("click", function (e) {
+        e.preventDefault();
+
+        let theNumber = document.getElementById("theNumber").value
+        console.log(theNumber);
+    });
+
+
+
+
+})();
